550255d0ff7826704c28bfbd032004c536e31388
Andy Yu <ayu303@gatech.edu>
2023-09-25 23:20:00 -0400

    Add Threading to client

--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import sys
 from socket import *
 import time
+import threading
 
 def serverThread(clientSocket: socket):
     """
-    Function to monitor the server separately from monitoring stdin
+    Function to monitor incoming server messages separately from monitoring stdin
     """
     return
 def main():
@@ -13,17 +14,22 @@ def main():
     PORT = int(args[2])
     password = args[3]
     name = args[4]
-    print(f"Connecting to to {HOST} on port {PORT}...\n")
+    print(f"Connecting to {HOST} on port {PORT}...")
     clientSocket = socket(AF_INET, SOCK_STREAM)
     clientSocket.connect((HOST, PORT))
-    clientSocket.send(f"{password}\n".encode())
-    time.sleep(10)
-    clientSocket.send(f"{name}\n".encode())
+    clientSocket.send(f"{password}".encode())
+    time.sleep(1)
+    clientSocket.send(f"{name}".encode())
     welcomeMessage = clientSocket.recv(1024)
     print(welcomeMessage.decode())
-
-
+    serverMonitor = threading.Thread(target=serverThread, 
+                                            args = ())
+    serverMonitor.daemon = True
+    serverMonitor.start()
     clientSocket.close()
+    #while True:
+        #time.sleep(1)
+
 
     
 
