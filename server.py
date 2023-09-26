4e98777833223b34610f2d8c06b8122143bd70f3
Andy Yu <ayu303@gatech.edu>
2023-09-25 23:21:35 -0400

    Server bugfix

--- server.py
+++ server.py
@@ -2,32 +2,48 @@ from socket import *
 import threading
 import sys
 
+def serverPrint(inStr: str):
+    print(inStr, end = '', flush = False)
+
 def clientThread(connectionSocket: socket, addr: str,
-                 password: str, nameSet: set, clientDict):
+                 password: str, nameSet: set, clientDict: dict):
     """
     operations for a single client
     """
     #check passcode
-    passwordAttempt = connectionSocket.recv(1024).decode().strip()
+    passwordAttempt = connectionSocket.recv(1024).decode()
     if passwordAttempt != password:
         #wrong password
         connectionSocket.close()
         return
-    name = connectionSocket.recv(1024).decode().strip()
+    name = connectionSocket.recv(1024).decode()
     if name in nameSet or " " in name:
         #name already exists or name has space  
         connectionSocket.close()
-        return
-    connectionSocket.send("Welcome!".encode())
+    connectionSocket.send("Welcome!\n".encode())
     nameSet.add(name)
-    joinAnnouncement = f"{name} joined the chatroom"
+    joinAnnouncement = f"{name} joined the chatroom\n"
     #announce to all other clients that a new person has joined the chatroom
-    print(joinAnnouncement, flush = True)
-
-    #remove name from set once client leaves
-    nameSet.remove(name)
-    return
-
+    serverPrint(joinAnnouncement)
+    while True:
+        msg = connectionSocket.recv(1024).decode()
+        #handle exit
+        if msg == ":Exit": 
+            #connectionSocket.send(msg.encode())
+            connectionSocket.close()
+            clientDict.pop(addr)
+            #remove name from set once client leaves
+            nameSet.remove(name)
+            #send message that user has left
+            leaveMsg = f"{name} has left the chatroom\n"
+            serverPrint(leaveMsg)
+            for client in clientDict:
+                clientDict[client].send(leaveMsg.encode())
+            return
+        outMsg = f"{name}: {msg}"
+        #send message to all clients
+        for client in clientDict:
+            clientDict[client].send(outMsg.encode())
 def main():
     args = sys.argv
     PORT = int(args[1])
@@ -35,18 +51,22 @@ def main():
     serverSocket = socket(AF_INET, SOCK_STREAM)
     serverSocket.bind(('', PORT))
     serverSocket.listen(1)
-    print(f"Server started on port {PORT}. Accepting connections...\n")
+    serverPrint(f"Server started on port {PORT}. Accepting connections...\n")
     clientDict = dict() #dict of all clients. Key is client address, value is the socket object 
     threadDict = dict() #dict of all threads. Key is client address, value is the thread it is running on
     nameSet = set() #set of all names on the server
     while True:
-        connectionSocket, addr = serverSocket.accept()
-        clientDict[addr] = connectionSocket
-        #start new thread
-        threadDict[addr] = threading.Thread(target=clientThread, 
-                                            args = (connectionSocket, addr, PASSWORD, nameSet, clientDict)) 
-        #allows killing the thread
-        threadDict[addr].daemon = True
-        threadDict[addr].start()
+        try:
+            connectionSocket, addr = serverSocket.accept()
+            clientDict[addr] = connectionSocket
+            #start new thread
+            threadDict[addr] = threading.Thread(target=clientThread, 
+                                                args = (connectionSocket, addr, PASSWORD, nameSet, clientDict)) 
+            #allows killing the thread
+            threadDict[addr].daemon = True
+            threadDict[addr].start()
+        except KeyboardInterrupt:
+            sys.exit()
+
 if __name__ == "__main__":
     main()
